6e32a36d3d86d89eae1bef1eea8dd49aa25c32f2
D371 Amamoriya <cirnoinwatertank@gmail.com>
2025-03-30 20:20:00 +0900

    Refactor

--- src/taikoi2t/main.py
+++ src/taikoi2t/main.py
@@ -18,6 +18,7 @@ from numpy import float64, int32, uint8
 
 type Image = numpy.typing.NDArray[uint8]
 type Result = tuple[list[tuple[int32, int32]], str, float64]
+type Bounding = tuple[int, int, int, int]
 
 # reader = easyocr.Reader(["ja", "en"])
 
@@ -26,43 +27,29 @@ THRESHOLD: int = 120
 
 def run() -> None:
     for path in sys.argv[1:]:
-        image: Image = imread(path)
-        if image is None:
+        source: Image = imread(path)
+        if source is None:
             continue
         print(f"=== {path} ===")
 
-        bounding = get_result_bounding(image)
-        if bounding is not None:
-            (left, top, right, bottom) = bounding
-            width: int = right - left
-            height: int = bottom - top
-            footerHeight: int = int(0.085 * height)
-            footerTop: int = int(bottom - footerHeight)
-            footer: Image = image[footerTop:bottom, left:right]
+        resultBounding = find_result_bounding(source)
+        if resultBounding is None:
+            continue
 
-            angle: float = math.tan(math.radians(14))
-            mat = numpy.array([[1, angle, 0], [0, 1, 0]], dtype=float64)
-            skewed: Image = warpAffine(
-                footer, mat, (int(width + footerHeight * angle), footerHeight)
-            )
+        studentsBounding = get_students_bounding(resultBounding)
+        studentsImage = preprocess_students(source, studentsBounding)
 
-            cv2.imshow(path, skewed)
-            cv2.waitKey()
-            cv2.destroyAllWindows()
+        show_image(path, studentsImage)
 
 
-def get_result_bounding(image: Image) -> tuple[int, int, int, int] | None:
+def find_result_bounding(image: Image) -> Bounding | None:
     RESULT_RATIO: float = 0.43
+    RATIO_EPS: float = 0.05
 
     imageWidth: int = image.shape[1]
-    imageHeight: int = image.shape[0]
-    print(f"width: {imageWidth}, height: {imageHeight}")
 
     grayImage: Image = cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret: int
-    wbImage: Image
-    ret, wbImage = threshold(grayImage, 0, 255, cv2.THRESH_OTSU)
-    print(f"threshold: {ret}")
+    wbImage: Image = threshold(grayImage, 0, 255, cv2.THRESH_OTSU)[1]
 
     contours, _ = findContours(wbImage, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -73,15 +60,36 @@ def get_result_bounding(image: Image) -> tuple[int, int, int, int] | None:
 
         [left, top, width, height] = bound
         ratio: float = height / width
-        if width > imageWidth / 2 and abs(ratio - RESULT_RATIO) < 0.01:
+        if width > imageWidth / 2 and abs(ratio - RESULT_RATIO) < RATIO_EPS:
             right: int = left + width
             bottom: int = top + height
-            # rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
-
-            print(f"bound width: {width}, height: {height}")
-            print(f"ratio: {ratio}")
-
             return (left, top, right, bottom)
+
+
+def get_students_bounding(bounding: Bounding) -> Bounding:
+    FOOTER_RATIO: float = 0.085
+    (left, top, right, bottom) = bounding
+    height: int = bottom - top
+    footerHeight = int(FOOTER_RATIO * height)
+    footerTop = int(bottom - footerHeight)
+    return (left, footerTop, right, bottom)
+
+
+def cutout_image(image: Image, bounding: Bounding) -> Image:
+    (left, top, right, bottom) = bounding
+    return image[top:bottom, left:right]
+
+
+def preprocess_students(source: Image, bounding: Bounding) -> Image:
+    SKEW_ANGLE: float = math.tan(math.radians(14))
+    SKEW_MAT = numpy.array([[1, SKEW_ANGLE, 0], [0, 1, 0]], dtype=float64)
+
+    (left, top, right, bottom) = bounding
+    width: int = right - left
+    height: int = bottom - top
+
+    footer: Image = cutout_image(source, bounding)
+    return warpAffine(footer, SKEW_MAT, (int(width + height * SKEW_ANGLE), height))
 
 
 def check() -> bool:
@@ -100,3 +108,9 @@ def load_image(path: str) -> bool:
 #     for result in results:
 #         print(result[1])
 #     return ", ".join(r[1] for r in results)
+
+
+def show_image(title: str, image: Image) -> None:
+    cv2.imshow(title, image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
