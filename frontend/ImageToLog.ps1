3f25dd62fc681fdb8c1d4f200992406212b37276
D371 Amamoriya <cirnoinwatertank@gmail.com>
2025-04-18 16:41:26 +0900

    Add backup images

--- frontend/ImageToLog.ps1
+++ frontend/ImageToLog.ps1
@@ -1,10 +1,19 @@
-$app_path = [System.Environment]::GetEnvironmentVariable("APP_PATH", "Process")
-$run_options = [System.Environment]::GetEnvironmentVariable("RUN_OPTIONS", "Process")
-$out_file = [System.Environment]::GetEnvironmentVariable("OUT_FILE", "Process")
+$appPath = [System.Environment]::GetEnvironmentVariable("APP_PATH", "Process")
+$runOptions = [System.Environment]::GetEnvironmentVariable("RUN_OPTIONS", "Process")
+$outFile = [System.Environment]::GetEnvironmentVariable("OUT_FILE", "Process")
+$backupDir = [System.Environment]::GetEnvironmentVariable("BACKUP_DIR", "Process")
 
-$files = Get-ChildItem -Path .\* -File -Include *.png, *.jpg | ForEach-Object { $_.Name }
-$command = "poetry --project $($app_path) run main -d .\students.csv $($run_options) " + ($files -join " ")
-$data = Invoke-Expression $command
+$files = Get-ChildItem -Path .\* -File -Include *.png, *.jpg
+$filenames = $files | ForEach-Object { $_.Name }
+$command = "poetry --project $($appPath) run main -d .\students.csv $($runOptions) " + ($filenames -join " ")
+$result = Invoke-Expression $command
 
-$data | Out-File -FilePath $out_file -Encoding utf8
-$data | Set-Clipboard
+$result | Out-File -FilePath $outFile -Encoding utf8
+$result | Set-Clipboard
+
+if (!(Test-Path $backupDir)) {
+    New-Item -ItemType Directory -Path $backupDir
+}
+foreach ($file in $files) {
+    Move-Item -Path $file.FullName -Destination $backupDir
+}
--- frontend/start-for-web-log.bat
+++ frontend/start-for-web-log.bat
@@ -1,5 +1,10 @@
 @echo off
+
 set APP_PATH=..
 set RUN_OPTIONS=
-set OUT_FILE=result.tsv
+set OUT_FILE=.\result.tsv
+set BACKUP_DIR=.\backup
+
+call .\user-settings.bat
+
 powershell -ExecutionPolicy Bypass -File ImageToLog.ps1
