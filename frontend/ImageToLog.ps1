fad8c3be079cd8b565d1dbeb31e6948a7ac4e91c
D371 Amamoriya <cirnoinwatertank@gmail.com>
2025-04-18 17:35:42 +0900

    Add fetching new screenshots

--- frontend/ImageToLog.ps1
+++ frontend/ImageToLog.ps1
@@ -1,10 +1,23 @@
+$imageFetchDir = [System.Environment]::GetEnvironmentVariable("IMAGE_FETCH_DIR", "Process")
 $appPath = [System.Environment]::GetEnvironmentVariable("APP_PATH", "Process")
 $runOptions = [System.Environment]::GetEnvironmentVariable("RUN_OPTIONS", "Process")
 $outFile = [System.Environment]::GetEnvironmentVariable("OUT_FILE", "Process")
 $backupDir = [System.Environment]::GetEnvironmentVariable("BACKUP_DIR", "Process")
 
-$files = Get-ChildItem -Path .\* -File -Include *.png, *.jpg
+$timeThreshold = (Get-Date).AddMinutes(-5)
+$fetchTargetFiles = Get-ChildItem -Path "$($imageFetchDir)\*" -File -Include *.png, *.jpg | 
+    Where-Object { $_.CreationTime -gt $timeThreshold }
+
+$currentDir = $PWD.Path
+foreach ($file in $fetchTargetFiles) {
+    Move-Item -Path $file.FullName -Destination $currentDir
+}
+
+# both images in the current directory already and fetched ones
+$files = Get-ChildItem -Path "$($currentDir)\*" -File -Include *.png, *.jpg
 $filenames = $files | ForEach-Object { $_.Name }
+$filenames | Write-Output
+
 $command = "poetry --project $($appPath) run main -d .\students.csv $($runOptions) " + ($filenames -join " ")
 $result = Invoke-Expression $command
 
--- frontend/start-for-web-log.bat
+++ frontend/start-for-web-log.bat
@@ -7,4 +7,6 @@ set BACKUP_DIR=.\backup
 
 call .\user-settings.bat
 
+set IMAGE_FETCH_DIR=%WEB_FETCH_DIR%
+
 powershell -ExecutionPolicy Bypass -File ImageToLog.ps1
