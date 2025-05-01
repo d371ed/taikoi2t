423660b126a7a31f61f5e5a307df2a19cda6653d
D371 Amamoriya <cirnoinwatertank@gmail.com>
2025-05-02 02:52:14 +0900

    Add testing for whole app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,37 @@
 from pathlib import Path
+from typing import List, Tuple
 
-from taikoi2t.app import error_line
+import pytest
+
+from taikoi2t.app import error_line, run
 from taikoi2t.args import Args
+
+
+def test_run(capsys: pytest.CaptureFixture[str]) -> None:
+    targets: List[Tuple[str, str]] = [
+        # sample screenshots are not contained in this repository
+        # use this section if you want to test your screenshots
+        # (
+        #     "app -d ./students.csv ./tests/images/0000.jpg",
+        #     "TRUE\tホシノ\t臨ホシノ\tシロコ＊\tノノミ\t水シロコ\t水アヤネ\t水ハナコ\tミヤコ\tシロコ＊\tホシノ\t水シロコ\tヒビキ\n",
+        # ),
+        # (
+        #     "app -d ./students.csv --csv ./tests/images/0007.png ./tests/images/0008.png",
+        #     "FALSE,モモイ,ホシノ,ミドリ,エイミ,水シロコ,リオ,ホシノ,ミヤコ,シロコ＊,イオリ,水シロコ,ナギサ\n"
+        #     + "TRUE,ホシノ,ハルナ,シロコ＊,アジュリ,水シロコ,サツキ,ホシノ,バネル,シュン,シロコ＊,水シロコ,カリン\n",
+        # ),
+        # (
+        #     "app -d ./students.csv ./tests/images/0009.png ./README.md ./tests/images/0010.png",
+        #     "TRUE\tホシノ\tバネル\tマリナ\tアジュリ\t水シロコ\tヒビキ\t水ハナコ\tマリナ\tシロコ＊\tホシノ\t水シロコ\tヒビキ\n"
+        #     + "FALSE\tError\tError\tError\tError\tError\tError\tError\tError\tError\tError\tError\tError\n"
+        #     + "TRUE\tシロコ＊\tシュン\tホシノ\tレイサ\t水シロコ\tサツキ\tホシノ\tレイサ\tミドリ\tシロコ＊\t水シロコ\tヒビキ\n",
+        # ),
+    ]
+    for command, expected in targets:
+        run(command.split())
+        captured = capsys.readouterr()
+        assert captured.out == expected
+        assert captured.err == ""
 
 
 def test_empty_tsv_line() -> None:
