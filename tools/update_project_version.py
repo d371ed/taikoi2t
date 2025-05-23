87910404922bfd11f910a0383638a50594cc6109
D371 Amamoriya <cirnoinwatertank@gmail.com>
2025-05-23 13:09:25 +0900

    Add pre-commit

--- tools/update_project_version.py
+++ tools/update_project_version.py
@@ -2,4 +2,15 @@ import subprocess
 
 from taikoi2t import TAIKOI2T_VERSION
 
-subprocess.run(["poetry", "version", TAIKOI2T_VERSION], check=True)
+
+def main() -> int:
+    try:
+        subprocess.run(["poetry", "version", TAIKOI2T_VERSION], check=True)
+        subprocess.run(["git", "add", "./pyproject.toml"], check=True)
+    except subprocess.CalledProcessError:
+        return 1
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
